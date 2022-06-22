76a0ff73320395cd08583b8f68bb14c79aa2d76b
mariadianacalugaru <calugaru.1893272@studenti.uniroma1.it>
2022-06-22 11:24:21 +0200

    classifica & contatti

--- containers/node/routes/index.js
+++ containers/node/routes/index.js
@@ -18,4 +18,92 @@ router.get('/', function(req, res, next) {
   }
 });
 
+/* GET contatti. */
+router.get('/contatti/', function(req, res, next) {
+  if (req.query.accesso) {
+    res.render('contatti',{
+      title: 'Trivia Stack | Contatti',
+      user: {
+        "username": "tamerhayek",
+        "password": "tamer2000"
+      }
+    });
+  } else {
+    res.render('contatti',{
+      title: 'Trivia Stack | Contatti'
+    });
+  }
+});
+
+/* GET classifica. */
+router.get('/classifica/', function(req, res, next) {
+  var array = [
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '34'
+    },
+    { 
+      'nome': 'Diana Calugaru',
+      'username': 'dianacalugaru',
+      'punteggio': '14'
+    },
+    { 
+      'nome': 'Andrea Bianchi',
+      'username': 'andreabianchi',
+      'punteggio': '3'
+    },
+    { 
+      'nome': 'Laura Marini',
+      'username': 'lauramarini',
+      'punteggio': '16'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '30'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '6'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '2'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '19'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '24'
+    },
+    { 
+      'nome': 'Tamer Hayek',
+      'username': 'tamerhayek',
+      'punteggio': '20'
+    }
+  ];
+  if (req.query.accesso) {
+    res.render('classifica',{
+      title: 'Trivia Stack | Classifica',
+      user: {
+        "username": "tamerhayek",
+        "password": "tamer2000"
+      },
+      'giocatori': array
+    });
+  } else {
+    res.render('classifica',{
+      title: 'Trivia Stack | Classifica',
+      'giocatori': array
+    });
+  }
+});
+
 module.exports = router;
